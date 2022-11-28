f6909d006e8b78142a93a2f5b491f1bb9293866b
fajar sidik priatna <jardik.oryza@gmail.com>
2022-11-28 14:53:47 +0700

    init

---
+++ Envoy.blade.php
@@ -0,0 +1,47 @@
+@servers(['production' => ['root@serve-ip-address']])
+ 
+@setup
+    $repo = 'https://github.com/jhonoryza/try-envoy.git';
+    $appDir = '/var/www';
+    $branch = 'master';
+
+    date_default_timezone_set('Asia/Jakarta');
+    $date = date('YmdHis');
+
+    $builds = $appDir . '/sources';
+    $deployment = $builds . '/' . $date;
+
+    $serve = $appDir . '/source';
+    $env = $appDir . '/.env';
+    $storage = $appDir . '/storage';
+@endsetup
+
+@story('deploy')
+    git
+    install
+    live
+@endstory
+
+@task('git', ['on' => 'production'])
+    git clone -b {{ $branch }} "{{ $repo }}" {{ $deployment }}
+@endtask
+
+@task('install', ['on' => 'production'])
+    cd {{ $deployment }}
+
+    rm -rf {{ $deployment }}/storage
+    
+    ln -nfs {{ $env }} {{ $deployment }}/.env
+    
+    ln -nfs {{ $storage }} {{ $deployment }}/storage
+
+    composer install --prefer-dist --no-dev
+    
+    php ./artisan migrate --force
+@endtask
+
+@task('live', ['on' => 'production'])
+    cd {{ $deployment }}
+    
+    ln -nfs {{ $deployment }} {{ $serve }}
+@endtask
